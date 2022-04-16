7746605b3a71f4bedfc5b564ff3cfdc0dc7e2342
yoloroy <slart2108@gmail.com>
2022-04-16 20:56:49 +0300

    :lipstick: fill header fonts

--- app/src/main/java/com/yoloroy/gameoflife/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/yoloroy/gameoflife/presentation/ui/theme/Type.kt
@@ -21,6 +21,9 @@ val Typography = Typography(
     h1 = TextStyle(fontFamily = pressStartFonts, fontSize = 96.sp, letterSpacing = (-1.5).sp),
     h2 = TextStyle(fontFamily = pressStartFonts, fontSize = 60.sp, letterSpacing = (-0.5).sp),
     h3 = TextStyle(fontFamily = pressStartFonts, fontSize = 48.sp, letterSpacing = 0.sp),
+    h4 = TextStyle(fontFamily = pressStartFonts, fontSize = 36.sp, letterSpacing = (-1).sp),
+    h5 = TextStyle(fontFamily = pressStartFonts, fontSize = 26.sp, letterSpacing = (-1).sp),
+    h6 = TextStyle(fontFamily = pressStartFonts, fontSize = 18.sp, letterSpacing = (-1).sp),
     subtitle1 = TextStyle(fontFamily = playFonts, fontSize = 24.sp, letterSpacing = 1.sp),
     subtitle2 = TextStyle(fontFamily = playFonts, fontSize = 20.sp, letterSpacing = 1.5.sp)
 )
