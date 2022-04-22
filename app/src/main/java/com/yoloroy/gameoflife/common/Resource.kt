54896d8570dbe278ff5cf377e40f22094d04f5c5
yoloroy <slart2108@gmail.com>
2022-04-22 14:54:52 +0300

    :boom: Resource

--- app/src/main/java/com/yoloroy/gameoflife/common/Resource.kt
+++ app/src/main/java/com/yoloroy/gameoflife/common/Resource.kt
@@ -1,7 +1,7 @@
 package com.yoloroy.gameoflife.common
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
-    class Loading<T>(data: T? = null) : Resource<T>(data)
+sealed class Resource<T, E>(val data: T? = null, val error: E? = null) {
+    class Success<T, E>(data: T) : Resource<T, E>(data)
+    class Error<T, E>(error: E, data: T? = null) : Resource<T, E>(data, error)
+    class Loading<T, E>(data: T? = null) : Resource<T, E>(data)
 }
