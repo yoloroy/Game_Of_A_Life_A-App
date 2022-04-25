58c37ee3eb0778567c5aefdfbbed75fb76ea5705
yoloroy <slart2108@gmail.com>
2022-04-25 09:18:55 +0300

    :construction: add Screens rotes class for future navigation

---
+++ app/src/main/java/com/yoloroy/gameoflife/presentation/util/Screen.kt
@@ -0,0 +1,12 @@
+package com.yoloroy.gameoflife.presentation.util
+
+sealed class Screen(val route: String) {
+    object LoginScreen : Screen("login")
+    object RegistrationScreen : Screen("registration")
+    object DreamsScreen : Screen("dreams")
+    object DreamsLibraryScreen : Screen("dreams library")
+    object DreamsDetailsScreen : Screen("dream details")
+    object ProfileScreen : Screen("profile")
+    object SettingsListScreen : Screen("settings list")
+    object ProfileSettingsScreen : Screen("settings profile")
+}
