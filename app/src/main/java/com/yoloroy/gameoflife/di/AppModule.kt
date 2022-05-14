1aef358a0b5da7e7e58a31848027efd936b4c538
yoloroy <slart2108@gmail.com>
2022-05-14 18:04:49 +0300

    :technologist: add repositories injections

---
+++ app/src/main/java/com/yoloroy/gameoflife/di/AppModule.kt
@@ -0,0 +1,37 @@
+package com.yoloroy.gameoflife.di
+
+import com.yoloroy.gameoflife.data.fake.*
+import com.yoloroy.gameoflife.domain.repository.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FakesReposModule {
+
+    @Provides
+    fun provideDreamDetailsRepository(): DreamDetailsRepository = DreamDetailsFakeRepository
+
+    @Provides
+    fun provideDreamsLibraryRepository(): DreamsLibraryRepository = DreamsLibraryFakeRepository
+
+    @Provides
+    fun provideDreamsRepository(): DreamsRepository = DreamsRepositoryFakeImpl
+
+    @Provides
+    fun provideLoginRepository(): LoginRepository = LoginRepositoryFakeImpl
+
+    @Provides
+    fun provideProfileSettingsRepository(): ProfileSettingsRepository = ProfileSettingsFakeRepository
+
+    @Provides
+    fun provideProfileRepository(): ProfileRepository = ProfileRepositoryFakeImpl
+
+    @Provides
+    fun provideRegistrationRepository(): RegistrationRepository = RegistrationRepositoryFakeImpl
+
+    @Provides
+    fun provideSettingsListRepository(): SettingsListRepository = SettingsListRepositoryFakeImpl
+}
