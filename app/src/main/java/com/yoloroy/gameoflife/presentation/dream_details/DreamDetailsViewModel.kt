1ee465a8e629490a2358c780dad4f3d086465375
yoloroy <slart2108@gmail.com>
2022-05-21 19:30:11 +0300

    :recycle: implement Use-Cases to DreamDetailsViewModel.kt

--- app/src/main/java/com/yoloroy/gameoflife/presentation/dream_details/DreamDetailsViewModel.kt
+++ app/src/main/java/com/yoloroy/gameoflife/presentation/dream_details/DreamDetailsViewModel.kt
@@ -7,19 +7,24 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yoloroy.gameoflife.common.Resource
-import com.yoloroy.gameoflife.domain.bad_repository.DreamDetailsRepository
 import com.yoloroy.gameoflife.domain.model.data.DreamDetail
-import com.yoloroy.gameoflife.presentation.dream_details.DreamDetailsMode.Adding
-import com.yoloroy.gameoflife.presentation.dream_details.DreamDetailsMode.Removing
+import com.yoloroy.gameoflife.domain.model.data.DreamStatus
+import com.yoloroy.gameoflife.domain.use_case.GetDreamCurrentStatus
+import com.yoloroy.gameoflife.domain.use_case.GetDreamDetail
+import com.yoloroy.gameoflife.domain.use_case.SubscribeOnDream
+import com.yoloroy.gameoflife.domain.use_case.UnsubscribeFromDream
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DreamDetailsViewModel @Inject constructor(
-    val repository: DreamDetailsRepository,
+    private val getDreamDetail: GetDreamDetail,
+    private val getDreamCurrentStatus: GetDreamCurrentStatus,
+    private val subscribeOnDream: SubscribeOnDream,
+    private val unsubscribeFromDream: UnsubscribeFromDream,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
@@ -32,25 +37,36 @@ class DreamDetailsViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            repository.getDreamById(dreamId)
+            getDreamDetail(dreamId)
                 .collect { dream = it }
         }
         viewModelScope.launch {
-            repository.getIsSubscribedOnDream(dreamId)
-                .transform<Boolean, DreamDetailsMode> { isSubscribed -> if (isSubscribed) Removing else Adding }
+            getDreamCurrentStatus(dreamId)
+                .map {
+                    when (it) {
+                        is Resource.Loading -> mode
+                        is Resource.Success -> when (it.data!!) {
+                            is DreamStatus.Subscribed -> DreamDetailsMode.Removing
+                            DreamStatus.Finished -> DreamDetailsMode.View
+                            is DreamStatus.InProcess -> DreamDetailsMode.Removing // TODO add "are you sure" dialog
+                            DreamStatus.None -> DreamDetailsMode.Adding
+                        }
+                        is Resource.Error -> DreamDetailsMode.View // TODO error catching
+                    }
+                }
                 .collect { mode = it }
         }
     }
 
-    fun addDream() {
+    fun addDream() { // TODO rename
         viewModelScope.launch {
-            repository.addDream(dreamId).collect() // TODO result handling
+            subscribeOnDream(dreamId).collect() // TODO result handling
         }
     }
 
-    fun removeDream() {
+    fun removeDream() { // TODO rename
         viewModelScope.launch {
-            repository.removeDream(dreamId).collect() // TODO result handling
+            unsubscribeFromDream(dreamId).collect() // TODO result handling
         }
     }
 }
