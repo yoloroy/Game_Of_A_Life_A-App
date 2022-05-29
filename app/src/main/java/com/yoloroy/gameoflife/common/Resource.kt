b07b686e8e3103c3e6bced1b21ca6057e7a98e43
yoloroy <slart2108@gmail.com>
2022-05-29 14:41:04 +0300

    :technologist: add no-null safety in Resource class

--- app/src/main/java/com/yoloroy/gameoflife/common/Resource.kt
+++ app/src/main/java/com/yoloroy/gameoflife/common/Resource.kt
@@ -3,11 +3,11 @@ package com.yoloroy.gameoflife.common
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-sealed class Resource<T>(val data: T? = null, val error: Throwable? = null) {
-    class Success<T>(data: T) : Resource<T>(data) {
+sealed class Resource<T>(open val data: T? = null, open val error: Throwable? = null) {
+    class Success<T>(override val data: T) : Resource<T>(data) {
         override fun toString() = "Success " + super.toString()
     }
-    class Error<T>(error: Throwable, data: T? = null) : Resource<T>(data, error) {
+    class Error<T>(override val error: Throwable, data: T? = null) : Resource<T>(data, error) {
         override fun toString() = "Error " + super.toString()
     }
     class Loading<T>(data: T? = null) : Resource<T>(data) {
@@ -18,8 +18,8 @@ sealed class Resource<T>(val data: T? = null, val error: Throwable? = null) {
 }
 
 inline fun <T, R> Resource<T>.transform(transformFun: (T) -> R) = when (this) {
-    is Resource.Success -> Resource.Success(transformFun(data!!))
-    is Resource.Error -> Resource.Error(error!!, data?.let { transformFun(it) })
+    is Resource.Success -> Resource.Success(transformFun(data))
+    is Resource.Error -> Resource.Error(error, data?.let { transformFun(it) })
     is Resource.Loading -> Resource.Loading(data?.let { transformFun(it) })
 }
 
