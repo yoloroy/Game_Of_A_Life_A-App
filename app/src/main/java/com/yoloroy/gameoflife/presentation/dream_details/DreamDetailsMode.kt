3b94382998aa0c3e26a26326b9a6cd891f14968b
yoloroy <slart2108@gmail.com>
2022-05-29 14:44:04 +0300

    :iphone: fix wrong removing icon in DreamDetailsMode

--- app/src/main/java/com/yoloroy/gameoflife/presentation/dream_details/DreamDetailsMode.kt
+++ app/src/main/java/com/yoloroy/gameoflife/presentation/dream_details/DreamDetailsMode.kt
@@ -2,11 +2,11 @@ package com.yoloroy.gameoflife.presentation.dream_details
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Add
-import androidx.compose.material.icons.sharp.Delete
+import androidx.compose.material.icons.sharp.Remove
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class DreamDetailsMode(val icon: ImageVector? = null) {
     Adding(Icons.Sharp.Add),
-    Removing(Icons.Sharp.Delete),
+    Removing(Icons.Sharp.Remove),
     View
 }
